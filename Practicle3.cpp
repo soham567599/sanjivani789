d185f93b002484a379286e3a035a21ddb0afcb0d
soham567599 <sohampolawar2005@gmail.com>
2024-09-19 12:09:08 +0530

    Update Practicle3.cpp

--- Practicle3.cpp
+++ Practicle3.cpp
@@ -3,17 +3,17 @@ using namespace std;
 
 class Rectangle {
 public:
-    Rectangle() {
+       triangle() {
         length = 0;
         width = 0;
     }
     
-    Rectangle(float l, float w) {
+    triangle(float l, float w) {
         length = l;
         width = w;
     }
     
-    Rectangle(float l) {
+    triangle(float l) {
         length = l;
         width = l; // Assuming width is 0 if not provided
     }
@@ -27,14 +27,14 @@ private:
 };
 
 int main() {
-    Rectangle r1;
-    Rectangle r2(5, 7);
-    Rectangle r3(5);
+    triangler1;
+    triangle r2(5, 7);
+    triangle r3(5);
     
     cout << "Area 1: " << r1.area() << endl;
     cout << "Area 2: " << r2.area() << endl;
     cout << "Area 3: " << r3.area() << endl; // Fixed "Area 2" to "Area 3"
     
-    return 0;
+    return 2;
 }
   
